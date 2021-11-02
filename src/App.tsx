468d1666977c8999e258c2b3df2f89f3e13c8f7e
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-11-02 23:44:25 +0000

    fix linter

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { BodyText } from 'components/Text'
 import { FormattedMessage } from 'react-intl'
 import { configure } from 'mobx'
 import IntlProvider from 'i18n/IntlProvider'
-import LanguageButtons from 'components/LanguageButtons'
 import Root from 'components/Root'
-import useApp from 'hooks/useApp'
 
 configure({
   enforceActions: 'never',
@@ -17,7 +15,6 @@ const App = () => {
         <BodyText>
           <FormattedMessage id="title" />
         </BodyText>
-        <LanguageButtons />
       </IntlProvider>
     </Root>
   )
