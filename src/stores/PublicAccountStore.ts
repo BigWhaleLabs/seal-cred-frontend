d9b4206baf39ae816b4f3cd878eda54e381fe568
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-12-12 18:43:42 -0800

    fix persistence of eth wallet

--- src/stores/PublicAccountStore.ts
+++ src/stores/PublicAccountStore.ts
@@ -6,6 +6,31 @@ import PersistableStore from 'stores/persistence/PersistableStore'
 class PublicAccountStore extends PersistableStore {
   mainEthWallet: Wallet = ethers.Wallet.createRandom()
   connectedIdentities: Identity[] = []
+
+  reviver = (key: string, value: unknown) => {
+    if (key === 'mainEthWallet') {
+      const mainEthWalletValue = value as {
+        address: string
+        privateKey: string
+      }
+      return new ethers.Wallet(mainEthWalletValue.privateKey)
+    }
+    return value
+  }
+
+  replacer = (key: string, value: unknown) => {
+    if (key === 'mainEthWallet') {
+      const mainEthWalletValue = value as {
+        address: string
+        privateKey: string
+      }
+      return {
+        address: mainEthWalletValue.address,
+        privateKey: mainEthWalletValue.privateKey,
+      }
+    }
+    return value
+  }
 }
 
 export default proxy(new PublicAccountStore()).makePersistent()
