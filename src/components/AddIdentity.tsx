d15a0536d13ce1bff9967ada0b1d04840e3f1826
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-12-14 10:53:41 -0800

    remove extra status

--- src/components/AddIdentity.tsx
+++ src/components/AddIdentity.tsx
@@ -6,7 +6,7 @@ export default function AddIdentity() {
   return (
     <Card>
       <BodyText>Link another identity</BodyText>
-      <Button disabled={status === 'connecting'} type="primary">
+      <Button type="primary">
         <img src="/img/metamask.svg" alt="metamask" />
         <span>Metamask</span>
       </Button>
