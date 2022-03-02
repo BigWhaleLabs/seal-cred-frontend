4b75f79b0aec52a8c516383fd4bb1b963358eda9
T-Damer <dpancov64@gmail.com>
2022-03-02 17:14:55 +0300

    fixed verify

--- src/stores/EthStore.ts
+++ src/stores/EthStore.ts
@@ -3,6 +3,8 @@ import { proxy } from 'valtio'
 import PersistableStore from 'stores/persistence/PersistableStore'
 import configuredModal from 'helpers/configuredModal'
 
+let provider: Web3Provider
+
 class EthStore extends PersistableStore {
   accounts: string[] | undefined = undefined
   ethLoading = false
@@ -12,10 +14,9 @@ class EthStore extends PersistableStore {
       this.ethLoading = true
 
       const instance = await configuredModal.connect()
-      const provider = new Web3Provider(instance)
-      const accounts = await provider.listAccounts()
+      provider = new Web3Provider(instance)
 
-      this.handleAccountChanged(accounts)
+      await this.handleAccountChanged()
       this.subscribeProvider(instance)
     } catch (error) {
       console.error(error)
@@ -24,21 +25,21 @@ class EthStore extends PersistableStore {
     }
   }
 
-  async getProvider() {
-    return new Web3Provider(await configuredModal.connect())
-  }
-
   async signMessage() {
+    if (!provider) return
+
     this.ethLoading = true
-    const provider = await this.getProvider()
     const signer = provider.getSigner()
     const signature = await signer.signMessage(await signer.getAddress())
     this.ethLoading = false
     return signature
   }
 
-  private handleAccountChanged(accounts: string[]) {
+  private async handleAccountChanged() {
+    if (!provider) return
+
     this.ethLoading = true
+    const accounts = await provider.listAccounts()
 
     if (accounts.length === 0) {
       this.accounts = undefined
@@ -50,20 +51,14 @@ class EthStore extends PersistableStore {
   }
 
   private subscribeProvider(provider: Web3Provider) {
-    if (!provider.on) return
+    if (!provider || !provider.on) return
 
     provider.on('error', (error: Error) => {
       console.error(error)
     })
-    provider.on('accountsChanged', (accounts: string[]) => {
-      this.handleAccountChanged(accounts)
-    })
-    provider.on('disconnect', (accounts: string[]) => {
-      this.handleAccountChanged(accounts)
-    })
-    provider.on('stop', (accounts: string[]) => {
-      this.handleAccountChanged(accounts)
-    })
+    provider.on('accountsChanged', () => this.handleAccountChanged())
+    provider.on('disconnect', () => this.handleAccountChanged())
+    provider.on('stop', () => this.handleAccountChanged())
   }
 }
 
