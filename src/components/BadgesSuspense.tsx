23197686377bc0f1389445fc17e10e0a24309eae
smyzin <smyzin.work@gmail.com>
2022-03-23 02:52:04 +0300

    Implement suspense to badges & update Fetching

---
+++ src/components/BadgesSuspense.tsx
@@ -0,0 +1,42 @@
+import { Suspense } from 'react'
+import {
+  alignItems,
+  classnames,
+  display,
+  flexDirection,
+  flexWrap,
+  gap,
+  justifyContent,
+} from 'classnames/tailwind'
+import BadgesStore from 'stores/BadgesStore'
+import FetchingData from 'components/FetchingData'
+import PublicBadges from 'components/PublicBadges'
+import useAddress from 'hooks/useAddress'
+
+const container = classnames(
+  display('flex'),
+  flexDirection('flex-row'),
+  flexWrap('flex-wrap'),
+  gap('gap-4'),
+  justifyContent('justify-center'),
+  alignItems('items-center')
+)
+
+function Badges() {
+  const address = useAddress()
+  BadgesStore.fetchPublicBadges(address)
+
+  return (
+    <Suspense
+      fallback={
+        <div className={container}>
+          <FetchingData text={'Fetching Badges ...'} />
+        </div>
+      }
+    >
+      <PublicBadges />
+    </Suspense>
+  )
+}
+
+export default Badges
