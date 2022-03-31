8146ed86f0090d32b9e82bca849322748f1ee8f0
Paul Oh <paul.oh1221@gmail.com>
2022-03-31 19:33:39 -0400

    Proof body modified

--- src/helpers/callProof.ts
+++ src/helpers/callProof.ts
@@ -6,17 +6,17 @@ import axios from 'axios'
 
 export default async function callProof(
   proof: MerkleProof | undefined,
-  ecdsaInput: EcdsaInput
+  ecdsaInput: EcdsaInput | undefined
 ) {
   const req: ProofBody = {
     root: proof?.root,
     leaf: proof?.leaf,
     siblings: proof?.siblings,
     pathIndices: proof?.pathIndices,
-    r: ecdsaInput.r,
-    s: ecdsaInput.s,
-    msghash: ecdsaInput.msghash,
-    pubkey: ecdsaInput.pubkey,
+    r: ecdsaInput?.r,
+    s: ecdsaInput?.s,
+    msghash: ecdsaInput?.msghash,
+    pubkey: ecdsaInput?.pubkey,
   }
 
   try {
