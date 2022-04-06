71ad55469a63386c291db2a9d9fc39e30465b887
smyzin <smyzin.work@gmail.com>
2022-04-07 00:14:09 +0300

    Add new feature

---
+++ src/models/BadgeToken.ts
@@ -0,0 +1,11 @@
+export type BadgeType = 'dosu1wave' | 'dosuInvites'
+
+export enum BadgeDescription {
+  'dosu1wave' = 'Dosu 1st wave',
+  'dosuInvites' = 'Dosu invite NFT',
+}
+
+export enum BadgeByContract {
+  'SCD' = 'dosu1wave',
+  'DOSU' = 'dosuInvites',
+}
