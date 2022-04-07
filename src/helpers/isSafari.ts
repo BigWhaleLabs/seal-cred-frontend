d09b761c05eab29131cd4a0ae19a93fd8fa91638
smyzin <smyzin.work@gmail.com>
2022-04-07 22:23:17 +0300

    Small fix

--- src/helpers/isSafari.ts
+++ src/helpers/isSafari.ts
@@ -1,5 +1,4 @@
 export default navigator.vendor &&
   navigator.vendor.includes('Apple') &&
-  navigator.userAgent &&
-  !navigator.userAgent.includes('CriOS') &&
-  !navigator.userAgent.includes('FxiOS')
+  !navigator.userAgent?.includes('CriOS') &&
+  !navigator.userAgent?.includes('FxiOS')
