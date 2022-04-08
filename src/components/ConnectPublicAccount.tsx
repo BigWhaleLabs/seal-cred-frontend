16c511f308dfed1fd29f284d0d86f29d33009669
Michel Erokhov <mixaileroxov@gmail.com>
2022-04-08 17:32:50 +0300

    update styles

--- src/components/ConnectPublicAccount.tsx
+++ src/components/ConnectPublicAccount.tsx
@@ -4,7 +4,23 @@ import { useSnapshot } from 'valtio'
 import CryptoWallet from 'components/CryptoWallet'
 import Loading from 'components/Loading'
 import PublicAccountStore from 'stores/PublicAccountStore'
+import classnames, {
+  backgroundColor,
+  cursor,
+  display,
+  padding,
+  whitespace,
+} from 'classnames/tailwind'
 import configuredModal from 'helpers/configuredModal'
+
+const accountConnectContainer = classnames(
+  padding('p-4'),
+  backgroundColor('bg-gray-50'),
+  display('flex'),
+  cursor('cursor-pointer')
+)
+
+const accountText = classnames(padding('px-4'), whitespace('whitespace-nowrap'))
 
 export default function ConnectPublicAccount() {
   const { ethLoading, ethError } = useSnapshot(PublicAccountStore)
@@ -15,22 +31,17 @@ export default function ConnectPublicAccount() {
     }
   }, [])
 
+  function onConnect() {
+    configuredModal.clearCachedProvider()
+    void PublicAccountStore.onConnect()
+  }
+
   return (
-    <div className="p-2 bg-gray-50">
-      <span className="flow-root px-2 py-2 transition duration-150 ease-in-out rounded-md hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50">
-        <span
-          onClick={async () => {
-            configuredModal.clearCachedProvider()
-            await PublicAccountStore.onConnect()
-          }}
-          className="flex items-center cursor-pointer width-auto"
-        >
-          {ethLoading && <Loading />}
-          <CryptoWallet />
-          <span className="px-4 whitespace-nowrap">Connect wallet</span>
-          {ethError && <ErrorText>{ethError}</ErrorText>}
-        </span>
-      </span>
+    <div className={accountConnectContainer} onClick={onConnect}>
+      {ethLoading && <Loading />}
+      <CryptoWallet />
+      <span className={accountText}>Connect wallet</span>
+      {ethError && <ErrorText>{ethError}</ErrorText>}
     </div>
   )
 }
--- src/components/CopyPrivateKey.tsx
+++ src/components/CopyPrivateKey.tsx
@@ -1,18 +1,57 @@
 import CopyWallet from 'components/CopyWallet'
 import PublicAccountStore from 'stores/PublicAccountStore'
+import classnames, {
+  alignItems,
+  backgroundColor,
+  borderRadius,
+  cursor,
+  display,
+  flexDirection,
+  fontSize,
+  fontWeight,
+  justifyContent,
+  padding,
+  textAlign,
+  textColor,
+  transitionProperty,
+  wordBreak,
+} from 'classnames/tailwind'
 import copy from 'copy-to-clipboard'
+
+const privatekeyBackground = classnames(
+  display('flex'),
+  flexDirection('flex-row'),
+  justifyContent('justify-between'),
+  transitionProperty('transition-colors'),
+  borderRadius('rounded-full'),
+  backgroundColor('bg-accent-light'),
+  textAlign('text-center'),
+  wordBreak('break-all'),
+  padding('py-4', 'px-6'),
+  cursor('cursor-pointer'),
+  textColor('text-primary'),
+  alignItems('items-center')
+)
+
+const privatekeyText = classnames(
+  padding('pr-4'),
+  fontWeight('font-bold'),
+  fontSize('text-lg')
+)
 
 export default function CopyPrivateKey() {
   if (!PublicAccountStore.privateKey) {
     return null
   }
 
+  function onCopy() {
+    copy(PublicAccountStore.privateKey ?? '')
+  }
+
   return (
-    <span
-      className="cursor-pointer"
-      onClick={() => copy(PublicAccountStore.privateKey ?? '')}
-    >
+    <div className={privatekeyBackground} onClick={onCopy}>
+      <span className={privatekeyText}>••••••••</span>
       <CopyWallet />
-    </span>
+    </div>
   )
 }
