93fea4a7872f5d0821bd6ae087e70f5ef006fecd
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-11 18:53:02 -0700

    bring back the proof generation

--- src/components/TokenList.tsx
+++ src/components/TokenList.tsx
@@ -101,50 +101,22 @@ export const TokenList = () => {
               setLoadingMint(true)
               setError(Errors.clear)
               try {
-                // setLoadingStage(LoadingStage.sign)
-                // const signature = await EthStore.signMessage(
-                //   PublicAccountStore.mainEthWallet.address
-                // )
-                // console.log('Signature', signature)
+                setLoadingStage(LoadingStage.sign)
+                const signature = await EthStore.signMessage(
+                  PublicAccountStore.mainEthWallet.address
+                )
+                console.log('Signature', signature)
 
-                // setLoadingStage(LoadingStage.proof)
-                // const treeProof = await createTreeProof()
-                // console.log('Merkle proof', treeProof)
+                setLoadingStage(LoadingStage.proof)
+                const treeProof = await createTreeProof()
+                console.log('Merkle proof', treeProof)
 
-                // setLoadingStage(LoadingStage.ecdsa)
-                // const ecdsaInput = await createEcdsaInput()
-                // console.log('ECDSA input', ecdsaInput)
+                setLoadingStage(LoadingStage.ecdsa)
+                const ecdsaInput = await createEcdsaInput()
+                console.log('ECDSA input', ecdsaInput)
 
                 setLoadingStage(LoadingStage.output)
-                // const proof = await callProof(treeProof, ecdsaInput)
-                const proof: ProofResponse = {
-                  proof: {
-                    pi_a: [
-                      '3678848276176002336481436659418248944719488774622732651645224676080149988055',
-                      '8447829545654419639111127746900423493127203351556922845651839437427735438458',
-                    ],
-                    pi_b: [
-                      [
-                        '13187899225836651088458614951216557283743281205360157867427792470839273369355',
-                        '9870117483971353363995826856454303009291271438169379330884987781981184709501',
-                      ],
-                      [
-                        '10346468289248965352074056335060368078501396390618828681102674029728259569267',
-                        '1429813900078326658402633009668067381719856544197650789550821656151113423434',
-                      ],
-                    ],
-                    pi_c: [
-                      '1359083388171014812393177602087310445137107279188003677500455040645225711492',
-                      '5697714956698750298815314528057628958739777499142434456041697240180425220429',
-                    ],
-                    protocol: 'groth16',
-                    curve: 'bn128',
-                  },
-                  publicSignals: [
-                    '1',
-                    '5996648938077548996564300724299923339597259625825626499011021328422106749444',
-                  ],
-                }
+                const proof = await callProof(treeProof, ecdsaInput)
                 if (!proof.proof) throw Errors.invalidProof
                 console.log('Proof', proof)
 
