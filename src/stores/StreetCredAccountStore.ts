8d85aaa6c64fa66ca1749f56cd37c783dc8aedaa
Michel Erokhov <mixaileroxov@gmail.com>
2022-04-27 18:22:33 +0300

    update AccountStore

--- src/stores/StreetCredAccountStore.ts
+++ src/stores/StreetCredAccountStore.ts
@@ -3,42 +3,19 @@ import {
   getDerivativeContracts,
   getOriginalContracts,
 } from 'helpers/fetchTokens'
-import Ledger from 'types/Ledger'
 import StreetCredStore from 'stores/StreetCredStore'
 import WalletStore from 'stores/WalletStore'
 
-async function fetchOriginalTokens({
-  ledger,
-  account,
-}: {
-  ledger: Ledger
-  account?: string
-}) {
-  const tokens = await getOriginalContracts(ledger, account)
-  return Promise.all(tokens.map((ctx) => ctx?.name()))
-}
-
-async function fetchDerivativeTokens({
-  ledger,
-  account,
-}: {
-  ledger: Ledger
-  account?: string
-}) {
-  const tokens = await getDerivativeContracts(ledger, account)
-  return Promise.all(tokens.map((ctx) => ctx?.name()))
-}
-
 export default derive({
   originalOwnedTokens: async (get) =>
-    fetchOriginalTokens({
-      ledger: await get(StreetCredStore).ledger,
-      account: get(WalletStore).account,
-    }),
+    getOriginalContracts(
+      await get(StreetCredStore).ledger,
+      get(WalletStore).account
+    ),
 
   derivativeOwnerTokens: async (get) =>
-    fetchDerivativeTokens({
-      ledger: await get(StreetCredStore).ledger,
-      account: get(WalletStore).account,
-    }),
+    getDerivativeContracts(
+      await get(StreetCredStore).ledger,
+      get(WalletStore).account
+    ),
 })
