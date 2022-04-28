f853eb233b77ecf01ad2cc0fe113dc4f2e0df758
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-28 09:21:49 -0700

    add derivative contracts owned

---
+++ src/components/DerivativeContractsOwned.tsx
@@ -0,0 +1,33 @@
+import { BodyText, SubheaderText } from 'components/Text'
+import { Suspense } from 'react'
+import { useSnapshot } from 'valtio'
+import ContractListContainer from 'components/ContractListContainer'
+import ContractName from 'components/ContractName'
+import StreetCredStore from 'stores/StreetCredStore'
+
+function ContractList() {
+  const { derivativeContracts } = useSnapshot(StreetCredStore)
+  return (
+    <>
+      {derivativeContracts?.owned?.length ? (
+        <ContractListContainer>
+          {derivativeContracts.owned.map((contract) => (
+            <ContractName key={contract.address} address={contract.address} />
+          ))}
+        </ContractListContainer>
+      ) : (
+        <SubheaderText>You don't have any derivative tokens yet.</SubheaderText>
+      )}
+    </>
+  )
+}
+
+export default function DerivativeContractsOwned() {
+  return (
+    <Suspense
+      fallback={<BodyText>Fetching derivative tokens owned by you...</BodyText>}
+    >
+      <ContractList />
+    </Suspense>
+  )
+}
