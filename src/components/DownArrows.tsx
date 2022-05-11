3c47c5c0f949a535b29895bc51fa1cf022220b40
T-Damer <dpancov64@gmail.com>
2022-05-11 14:06:16 +0300

    make arrows good again

--- src/components/DownArrows.tsx
+++ src/components/DownArrows.tsx
@@ -1,23 +1,10 @@
 import Arrow from 'components/Arrow'
 import CardSeparator from 'components/CardSeparator'
-import classnames, {
-  animation,
-  margin,
-  space,
-  transitionProperty,
-} from 'classnames/tailwind'
-
-const arrowsWrapper = classnames(
-  transitionProperty('transition-all'),
-  animation('animate-bounce'),
-  space('space-y-4'),
-  margin('my-10')
-)
 
 const DownArrows = () => {
   return (
     <div>
-      <div className={arrowsWrapper}>
+      <div className="transition-all animate-bounce -space-y-4 my-10">
         <Arrow flip disabled />
         <Arrow flip disabled />
         <Arrow flip disabled />
