97f8fd531a8955b0cac2519af1d03688d0753cd3
smyzin <smyzin.work@gmail.com>
2022-05-11 17:19:23 +0300

    Updates according to comments

--- src/components/ToolTip.tsx
+++ src/components/ToolTip.tsx
@@ -15,7 +15,7 @@ const tooltipClasses = (xs: boolean) =>
   classnames(
     backgroundColor('bg-white'),
     maxWidth(xs ? 'max-w-xs' : 'max-w-sm'),
-    inset(xs ? '!left-0' : null),
+    inset(xs ? '!left-0' : undefined),
     opacity('!opacity-100'),
     borderRadius('!rounded-lg')
   )
@@ -26,17 +26,19 @@ const ToolTip: FC<{
   clickable?: boolean
 }> = ({ place, dataFor, clickable }) => {
   const { xs, sm } = useBreakpoints()
+  const xsOnly = xs && !sm
+
   return (
     <TooltipText>
       <ReactTooltip
-        place={xs || !sm ? 'top' : place}
+        place={xsOnly ? 'top' : place}
         data-for={dataFor}
         effect="solid"
         backgroundColor={backgroundColor('bg-white')}
         textColor={textColor('text-blue-900')}
         arrowColor={backgroundColor('bg-white')}
         clickable={clickable}
-        className={tooltipClasses(xs && !sm)}
+        className={tooltipClasses(xsOnly)}
       />
     </TooltipText>
   )
