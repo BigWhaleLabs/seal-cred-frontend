febce3b94ab9186fd1f1c0e240b19fc98de9dc0e
smyzin <smyzin.work@gmail.com>
2022-05-13 23:16:02 +0300

    Remove console logging

--- src/helpers/truncateMiddle.ts
+++ src/helpers/truncateMiddle.ts
@@ -3,6 +3,5 @@ export function truncateMiddle(str: string, from = 11, to?: number): string {
 }
 
 export function truncateIfNeeded(str: string, maxLength: number): string {
-  console.log(str, str.length, maxLength)
   return str.length > maxLength ? `${str.slice(0, maxLength)}...` : str
 }
