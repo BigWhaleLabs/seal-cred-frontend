6ea7ddc7c00306dbd8e66051afa8397ca160916d
smyzin <smyzin.work@gmail.com>
2022-05-13 23:28:12 +0300

    Fix lint error

--- src/components/ENSAddress.tsx
+++ src/components/ENSAddress.tsx
@@ -4,20 +4,20 @@ import { truncateIfNeeded } from 'helpers/truncateMiddle'
 import EnsStore from 'stores/EnsStore'
 import useEnsNameOrAddress from 'helpers/useEnsNameOrAddress'
 
-function ENSAddressSuspender({ address }: { address: string }) {
+function EnsAddressSuspender({ address }: { address: string }) {
   const ensNameOrAddress = useEnsNameOrAddress(address)
 
   return <span>{truncateIfNeeded(ensNameOrAddress, 17)}</span>
 }
 
-const ENSAddress: FC<{ address: string }> = ({ address }) => {
+const EnsAddress: FC<{ address: string }> = ({ address }) => {
   EnsStore.fetchEnsName(address)
 
   return (
     <Suspense fallback={<>Looking for ens...</>}>
-      <ENSAddressSuspender address={address} />
+      <EnsAddressSuspender address={address} />
     </Suspense>
   )
 }
 
-export default ENSAddress
+export default EnsAddress
