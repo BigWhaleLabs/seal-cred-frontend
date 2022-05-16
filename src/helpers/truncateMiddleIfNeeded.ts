16e954ab28fb8f55f2f638461bc71ed3819745cf
smyzin <smyzin.work@gmail.com>
2022-05-16 15:46:16 +0300

    Fixes according to comments

--- src/helpers/truncateMiddleIfNeeded.ts
+++ src/helpers/truncateMiddleIfNeeded.ts
@@ -1,8 +1,7 @@
 export default function (str: string, maxLength: number) {
+  const isOddNum = maxLength % 2 === 1 ? 1 : 2
+  const halfLength = maxLength / 2
   return str.length > maxLength && maxLength > 4
-    ? `${str.slice(
-        0,
-        maxLength / 2 - (maxLength % 2 === 1 ? 1 : 2)
-      )}...${str.slice(-(maxLength / 2 - 1))}`
+    ? `${str.slice(0, halfLength - isOddNum)}...${str.slice(-(halfLength - 1))}`
     : str
 }
