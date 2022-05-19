ccb4562b0a0e20de3a41beb5c74482652a5da6b0
T-Damer <dpancov64@gmail.com>
2022-05-19 16:20:17 +0300

    hfix

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -5,7 +5,7 @@ import {
   SCLEDGER_CONTRACT_ADDRESS,
 } from '@big-whale-labs/constants'
 
-export default {
+const env = {
   VITE_ENCRYPT_KEY: import.meta.env.VITE_ENCRYPT_KEY as string,
   VITE_FORTMATIC_KEY:
     (import.meta.env.VITE_FORTMATIC_KEY as string) || FORTMATIC_KEY,
@@ -16,6 +16,7 @@ export default {
     (import.meta.env.VITE_SCLEDGER_CONTRACT_ADDRESS as string) ||
     SCLEDGER_CONTRACT_ADDRESS,
   VITE_VERIFY_URL: import.meta.env.VITE_VERIFY_URL as string,
+}
 
 Object.values(env).forEach((value: string, index) => {
   if (value === '' || value === undefined || value === null)
