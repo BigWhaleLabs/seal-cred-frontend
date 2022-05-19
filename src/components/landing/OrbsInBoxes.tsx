b7ec26fe4becfd7b581293be99190f20857cbe81
Tuan Dau <tukauu@gmail.com>
2022-05-19 14:52:10 -0400

    refactored models/colors.ts
    * deleted models/colors.ts
    
    * added models/color.ts which specifies the allowed colors for landing components
    
    * added 3 helper functions to convert from the color type to stroke/bg/drop-shadow
    
    * deleted any references to hex color enums in favor of tailwind classes

--- src/components/landing/OrbsInBoxes.tsx
+++ src/components/landing/OrbsInBoxes.tsx
@@ -1,5 +1,5 @@
 import { HighlightedText } from 'components/Text'
-import Colors, { colorToTailwindBg } from 'models/Colors'
+import Color from 'models/Color'
 import OrbBox from 'icons/OrbBox'
 import ZkSphere from 'components/landing/ZkSphere'
 import classnames, {
@@ -16,6 +16,7 @@ import classnames, {
   transitionProperty,
   zIndex,
 } from 'classnames/tailwind'
+import colorToTailwindBackground from 'helpers/colorToTailwindBackground'
 import useScrollPercent from 'hooks/useScrollPercent'
 
 const orbBoxes = classnames(
@@ -33,11 +34,11 @@ const orbBox = (margins?: TMargin) =>
     space('space-y-2'),
     margin(margins)
   )
-const highlightedWrapper = (color: Colors) =>
+const highlightedWrapper = (color: Color) =>
   classnames(
     padding('px-2', 'py-1'),
     borderRadius('rounded-full'),
-    backgroundColor(colorToTailwindBg(color)),
+    backgroundColor(colorToTailwindBackground(color)),
     zIndex('z-10')
   )
 
@@ -48,29 +49,29 @@ export default function () {
   return (
     <div className={orbBoxes} style={{ transform: 'translateY(5.625rem)' }}>
       <div className={orbBox('mt-8')}>
-        <OrbBox color={Colors.tertiary} shadow={animNotStarted} />
+        <OrbBox color="tertiary" shadow={animNotStarted} />
         <div className={position('absolute')}>
-          <ZkSphere color={Colors.tertiary} animated />
+          <ZkSphere color="tertiary" animated />
         </div>
-        <div className={highlightedWrapper(Colors.tertiary)}>
+        <div className={highlightedWrapper('tertiary')}>
           <HighlightedText center>Wallet 01</HighlightedText>
         </div>
       </div>
       <div className={orbBox()}>
-        <OrbBox color={Colors.accent} shadow={animNotStarted} />
+        <OrbBox color="accent" shadow={animNotStarted} />
         <div className={position('absolute')}>
-          <ZkSphere color={Colors.accent} animated />
+          <ZkSphere color="accent" animated />
         </div>
-        <div className={highlightedWrapper(Colors.accent)}>
+        <div className={highlightedWrapper('accent')}>
           <HighlightedText center>Wallet 02</HighlightedText>
         </div>
       </div>
       <div className={orbBox('mt-11')}>
-        <OrbBox color={Colors.secondary} shadow={animNotStarted} />
+        <OrbBox color={'secondary'} shadow={animNotStarted} />
         <div className={position('absolute')}>
-          <ZkSphere color={Colors.secondary} animated />
+          <ZkSphere color={'secondary'} animated />
         </div>
-        <div className={highlightedWrapper(Colors.secondary)}>
+        <div className={highlightedWrapper('secondary')}>
           <HighlightedText center>Wallet 03</HighlightedText>
         </div>
       </div>
--- src/icons/OrbBox.tsx
+++ src/icons/OrbBox.tsx
@@ -1,34 +1,34 @@
-import Colors, { colorToDropShadow } from 'models/Colors'
+import Color from 'models/Color'
 import classnames, {
-  TDropShadow,
-  dropShadow,
   fill,
   opacity,
   transitionProperty,
 } from 'classnames/tailwind'
+import colorToDropShadow from 'helpers/colorToDropShadow'
+import colorToStrokeColor from 'helpers/colorToStrokeColor'
 
-const orbBoxStyles = (shadowColor: TDropShadow, shadow?: boolean) =>
+const orbBoxStyles = (shadow?: boolean) =>
   classnames(
-    dropShadow(shadowColor),
     opacity(shadow ? 'opacity-100' : 'opacity-90'),
     transitionProperty('transition-all')
   )
 const fillPath = fill('fill-primary-dark')
 
 interface OrbBoxProps {
-  color: Colors
+  color: Color
   shadow?: boolean
 }
 
 export default function OrbBox({ color, shadow }: OrbBoxProps) {
   const shadowColor = colorToDropShadow(color)
+  const strokeColor = colorToStrokeColor(color)
 
   return (
     <div style={{ height: '4.063rem', width: '4.063rem' }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 66 66"
-        className={orbBoxStyles(shadowColor, shadow)}
+        className={classnames(shadowColor, orbBoxStyles(shadow))}
       >
         <path
           d="M3.77,11.72a8.67,8.67,0,0,1,8.66-8.67H60.1a8.68,8.68,0,0,1,8.67,8.67V59.38a8.68,8.68,0,0,1-8.67,8.67H12.43a8.67,8.67,0,0,1-8.66-8.67Z"
@@ -39,25 +39,27 @@ export default function OrbBox({ color, shadow }: OrbBoxProps) {
           d="M3.77,11.72a8.67,8.67,0,0,1,8.66-8.67H60.1a8.68,8.68,0,0,1,8.67,8.67V59.38a8.68,8.68,0,0,1-8.67,8.67H12.43a8.67,8.67,0,0,1-8.66-8.67Z"
           transform="translate(-3.27 -2.55)"
           fill="none"
-          stroke={color}
+          className={strokeColor}
         />
         <path
           d="M63.35,8.47V58.3A4.33,4.33,0,0,1,59,62.63H36.27m-27.09,0V12.8a4.33,4.33,0,0,1,4.34-4.33H36.27"
           transform="translate(-3.27 -2.55)"
           fill="none"
-          stroke={color}
+          stroke={strokeColor}
+          className={strokeColor}
         />
-        <circle cx="33" cy="60.08" r="1.08" fill={color} />
-        <circle cx="5.92" cy="60.08" r="1.08" fill={color} />
-        <circle cx="33" cy="5.92" r="1.08" fill={color} />
-        <circle cx="60.08" cy="5.92" r="1.08" fill={color} />
+        <circle cx="33" cy="60.08" r="1.08" fill={strokeColor} />
+        <circle cx="5.92" cy="60.08" r="1.08" fill={strokeColor} />
+        <circle cx="33" cy="5.92" r="1.08" fill={strokeColor} />
+        <circle cx="60.08" cy="5.92" r="1.08" fill={strokeColor} />
         <line
           x1="60.08"
           y1="29.21"
           x2="65.5"
           y2="29.21"
           fill="none"
-          stroke={color}
+          stroke={strokeColor}
+          className={strokeColor}
         />
         <line
           x1="0.5"
@@ -65,7 +67,8 @@ export default function OrbBox({ color, shadow }: OrbBoxProps) {
           x2="5.92"
           y2="29.21"
           fill="none"
-          stroke={color}
+          stroke={strokeColor}
+          className={strokeColor}
         />
         <line
           x1="16.21"
@@ -73,7 +76,8 @@ export default function OrbBox({ color, shadow }: OrbBoxProps) {
           x2="16.21"
           y2="5.92"
           fill="none"
-          stroke={color}
+          stroke={strokeColor}
+          className={strokeColor}
         />
         <line
           x1="50.87"
@@ -81,7 +85,8 @@ export default function OrbBox({ color, shadow }: OrbBoxProps) {
           x2="50.87"
           y2="65.5"
           fill="none"
-          stroke={color}
+          stroke={strokeColor}
+          className={strokeColor}
         />
       </svg>
     </div>
