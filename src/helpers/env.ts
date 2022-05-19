40386e1db5514c31f03ed96ba53f390e187b4ec6
T-Damer <dpancov64@gmail.com>
2022-05-19 20:19:05 +0300

    used envalid library

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -4,19 +4,14 @@ import {
   FORTMATIC_KEY,
   SCLEDGER_CONTRACT_ADDRESS,
 } from '@big-whale-labs/constants'
-import { cleanEnv, str } from 'helpers/envalid'
-import handleError from 'helpers/handleError'
+import { cleanEnv, str } from 'envalid'
 
-export default cleanEnv(
-  import.meta.env,
-  {
-    VITE_ENCRYPT_KEY: str(),
-    VITE_FORTMATIC_KEY: str({ default: FORTMATIC_KEY }),
-    VITE_APP_NAME: str(),
-    VITE_ETH_NETWORK: str({ default: ETH_NETWORK }),
-    VITE_ETH_RPC: str({ default: ETH_RPC }),
-    VITE_SCLEDGER_CONTRACT_ADDRESS: str({ default: SCLEDGER_CONTRACT_ADDRESS }),
-    VITE_VERIFY_URL: str(),
-  },
-  { reporter: handleError }
-)
+export default cleanEnv(import.meta.env, {
+  VITE_ENCRYPT_KEY: str(),
+  VITE_FORTMATIC_KEY: str({ default: FORTMATIC_KEY }),
+  VITE_APP_NAME: str(),
+  VITE_ETH_NETWORK: str({ default: ETH_NETWORK }),
+  VITE_ETH_RPC: str({ default: ETH_RPC }),
+  VITE_SCLEDGER_CONTRACT_ADDRESS: str({ default: SCLEDGER_CONTRACT_ADDRESS }),
+  VITE_VERIFY_URL: str(),
+})
