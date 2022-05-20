4674b8e2c2b2c74e96dfc796177f19eaca162ae6
T-Damer <dpancov64@gmail.com>
2022-05-20 18:27:49 +0300

    updated throttle hook

--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,17 +1,20 @@
 import { useCallback, useRef } from 'react'
 
-export default function (callback: (...args: unknown[]) => void, delay = 20) {
+export default function (callback: (...args: unknown[]) => void, delayMs = 20) {
   const isThrottled = useRef(false)
 
   const throttledCallback = useCallback(
-    (...args) => {
+    (...args: unknown[]) => {
       if (isThrottled.current) return
 
       callback(args)
+
       isThrottled.current = true
-      setTimeout(() => (isThrottled.current = false), delay)
+      setTimeout(() => {
+        isThrottled.current = false
+      }, delayMs)
     },
-    [callback, delay]
+    [callback, delayMs]
   )
 
   return throttledCallback
