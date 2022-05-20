ed951e60f8e24f32f7dd477148a856bff199da9f
smyzin <smyzin.work@gmail.com>
2022-05-20 19:52:44 +0300

    Return the order of props

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -200,7 +200,7 @@ export function HighlightedText({
   bold?: boolean
   center?: boolean
 }) {
-  return <div className={highlightedText(center, bold)}>{children}</div>
+  return <div className={highlightedText(bold, center)}>{children}</div>
 }
 
 const sphereText = classnames(
