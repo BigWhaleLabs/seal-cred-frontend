032d1a7d0d24903a0a36fd95543a660b6bb57757
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-09 09:07:08 -0700

    fix build

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import 'index.css'
-import { injectStyle } from 'react-toastify/dist/inject-style'
+import 'react-toastify/dist/ReactToastify.css'
+
 import { render } from 'preact'
 import App from 'App'
-
-injectStyle()
 
 render(<App />, document.getElementById('root') as Element)
