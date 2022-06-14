d232f754217e6f0368dbd06674e977b616d19d27
Michel Erokhov <mixaileroxov@gmail.com>
2022-06-15 00:08:38 +0300

    filter out tokens that user transferred away

--- src/helpers/getOwnedERC721.ts
+++ src/helpers/getOwnedERC721.ts
@@ -9,27 +9,80 @@ const sig = 'Transfer(address,address,uint256)'
 const sigHash = utils.keccak256(utils.toUtf8Bytes(sig))
 
 export default async function (account: string) {
-  return (
-    await defaultProvider.getLogs({
-      fromBlock: 0,
-      toBlock: 'latest',
-      topics: [utils.id(sig), null, utils.hexZeroPad(account, 32)],
-    })
+  const eventsTo = await defaultProvider.getLogs({
+    fromBlock: 0,
+    toBlock: 'latest',
+    topics: [utils.id(sig), null, utils.hexZeroPad(account, 32)],
+  })
+
+  const eventsFrom = await defaultProvider.getLogs({
+    fromBlock: 0,
+    toBlock: 'latest',
+    topics: [utils.id(sig), utils.hexZeroPad(account, 32)],
+  })
+
+  const events = []
+  while (eventsTo.length > 0 || eventsFrom.length > 0) {
+    if (eventsTo.length === 0) {
+      events.push(eventsFrom.pop())
+      continue
+    }
+
+    if (eventsFrom.length === 0) {
+      events.push(eventsTo.pop())
+      continue
+    }
+
+    const lastTo = eventsTo[eventsTo.length - 1]
+    const lastFrom = eventsFrom[eventsFrom.length - 1]
+
+    if (lastTo.blockNumber > lastFrom.blockNumber) {
+      events.push(eventsTo.pop())
+      continue
+    }
+    if (lastTo.blockNumber < lastFrom.blockNumber) {
+      events.push(eventsFrom.pop())
+      continue
+    }
+
+    events.push(
+      lastTo.logIndex > lastFrom.logIndex ? eventsTo.pop() : eventsFrom.pop()
+    )
+  }
+
+  const parsedLogs = events.reverse().map((log) => {
+    if (!log) return null
+    if (log.topics[0] !== sigHash) return null
+    const data = log.data
+    const topics = log.topics
+    if (topics.length <= 3) return null
+    const result = iface.parseLog({ data, topics })
+
+    return {
+      contract: log.address,
+      transaction: log.transactionHash,
+      to: result.args.to,
+      from: result.args.from,
+      tokenId: result.args.tokenId.toString(),
+    }
+  })
+
+  const contractMap = parsedLogs.reduce(
+    (resultMap, info) =>
+      info && info.contract
+        ? {
+            ...resultMap,
+            [info.contract]:
+              typeof resultMap[info.contract] !== 'undefined'
+                ? resultMap[info.contract] ^ (info.tokenId + 1)
+                : info.tokenId + 1,
+          }
+        : resultMap,
+    {} as { [tokenAddress: string]: number }
   )
-    .map((log) => {
-      if (log.topics[0] !== sigHash) return null
-      const data = log.data
-      const topics = log.topics
-      if (topics.length <= 3) return null
-      const result = iface.parseLog({ data, topics })
-      return {
-        contract: log.address,
-        transaction: log.transactionHash,
-        to: result.args.to,
-        from: result.args.from,
-        tokenId: result.args.tokenId.toString(),
-      }
-    })
-    .map((o) => (o ? o.contract : null))
-    .filter((o) => o !== null) as string[]
+
+  return Object.entries(contractMap).reduce(
+    (chain, [key, value]) => (value > 0 ? [...chain, key] : chain),
+    [] as string[]
+  )
 }
