76dad8a302c6ee9b817c024925369722563302ab
Michel Erokhov <mixaileroxov@gmail.com>
2022-06-16 04:09:32 +0300

    filter out tokens that user transferred away (#222)
    * filter out tokens that user transferred away
    
    * add special rule
    
    * optimize
    
    * simplified
    
    * refactor
    
    * fix loop
    
    * update vars
    
    * removed redundant check
    
    Co-authored-by: smyzin <smyzin.work@gmail.com>

--- src/helpers/getOwnedERC721.ts
+++ src/helpers/getOwnedERC721.ts
@@ -1,7 +1,7 @@
 import { utils } from 'ethers'
 import defaultProvider from 'helpers/defaultProvider'
 
-const iface = new utils.Interface([
+const transferEventInterface = new utils.Interface([
   'event Transfer(address indexed from, address indexed to, uint indexed tokenId)',
 ])
 
@@ -9,27 +9,36 @@ const sig = 'Transfer(address,address,uint256)'
 const sigHash = utils.keccak256(utils.toUtf8Bytes(sig))
 
 export default async function (account: string) {
-  return (
-    await defaultProvider.getLogs({
-      fromBlock: 0,
-      toBlock: 'latest',
-      topics: [utils.id(sig), null, utils.hexZeroPad(account, 32)],
-    })
-  )
-    .map((log) => {
-      if (log.topics[0] !== sigHash) return null
-      const data = log.data
-      const topics = log.topics
-      if (topics.length <= 3) return null
-      const result = iface.parseLog({ data, topics })
-      return {
-        contract: log.address,
-        transaction: log.transactionHash,
-        to: result.args.to,
-        from: result.args.from,
-        tokenId: result.args.tokenId.toString(),
-      }
-    })
-    .map((o) => (o ? o.contract : null))
-    .filter((o) => o !== null) as string[]
+  const receivedLogs = await defaultProvider.getLogs({
+    fromBlock: 0,
+    toBlock: 'latest',
+    topics: [utils.id(sig), null, utils.hexZeroPad(account, 32)],
+  })
+
+  const sentLogs = await defaultProvider.getLogs({
+    fromBlock: 0,
+    toBlock: 'latest',
+    topics: [utils.id(sig), utils.hexZeroPad(account, 32)],
+  })
+
+  const ownedTokens: { [token: string]: number } = {}
+
+  for (const { topics, data, address } of receivedLogs.concat(sentLogs)) {
+    if (topics[0] !== sigHash || topics.length <= 3) continue
+
+    const {
+      args: { tokenId },
+    } = transferEventInterface.parseLog({ data, topics })
+
+    const value = (tokenId + 1).toString()
+
+    if (ownedTokens[address]) {
+      ownedTokens[address] ^= value
+      if (!ownedTokens[address]) delete ownedTokens[address]
+    } else {
+      ownedTokens[address] = value
+    }
+  }
+
+  return Object.keys(ownedTokens)
 }
