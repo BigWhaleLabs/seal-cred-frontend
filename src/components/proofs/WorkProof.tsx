2d40223ac574cf9e11d052095d4b1ef80c8f0416
Michel Erokhov <mixaileroxov@gmail.com>
2022-06-23 01:51:33 +0300

    Fix components (#256)
    * fix ProofSection, Line, WorkProof
    
    * remove unused border-1

--- src/components/proofs/WorkProof.tsx
+++ src/components/proofs/WorkProof.tsx
@@ -42,7 +42,7 @@ const proofLineContainer = classnames(
 
 export default function () {
   const [open, setOpen] = useState(true)
-  const [email, setEmail] = useState<string>()
+  const [email, setEmail] = useState<string | undefined>()
 
   const domain = email ? email.split('@')[1] : ''
 
@@ -59,39 +59,38 @@ export default function () {
   }
 
   return (
-    <Line className={proofLineContainer}>
-      <div className={workTitleContainer}>
-        <span>{domain ? `Work domain @${domain}` : `Work email`}</span>
-        <button className={arrowContainer} onClick={onToggle}>
-          {!open && !domain && <span>Get started</span>}
-          <Arrow disabled vertical turnDown={open} />
-        </button>
+    <Line>
+      <div className={proofLineContainer}>
+        <div className={workTitleContainer}>
+          <span>{domain ? `Work domain @${domain}` : `Work email`}</span>
+          <button className={arrowContainer} onClick={onToggle}>
+            {!open && !domain && <span>Get started</span>}
+            <Arrow disabled vertical turnDown={open} />
+          </button>
+        </div>
+        {open && (
+          <>
+            <BadgeText>
+              {domain
+                ? `A token has been sent to ${email}. Copy the token and add it here to create zk proof. Or re-enter email.`
+                : `Add your work email and we’ll send you a token for that email. Then, use the token here to create zk proof.`}
+            </BadgeText>
+            {domain ? (
+              <TextForm
+                submitText="Generate proof"
+                placeholder="Paste token here"
+                onSubmit={onSendSecret}
+              />
+            ) : (
+              <EmailForm
+                submitText="Submit email"
+                placeholder="Work email..."
+                onSubmit={onSendEmail}
+              />
+            )}
+          </>
+        )}
       </div>
-      {open && (
-        <>
-          <BadgeText>
-            {domain
-              ? `A token has been sent to ${email}. Copy the token and add it here to create zk proof. Or re-enter email.`
-              : `
-                        Add your work email and we’ll send you a token for that email. Then,
-                        use the token here to create zk proof.
-            `}
-          </BadgeText>
-          {domain ? (
-            <TextForm
-              submitText="Generate proof"
-              placeholder="Paste token here"
-              onSubmit={onSendSecret}
-            />
-          ) : (
-            <EmailForm
-              submitText="Submit email"
-              placeholder="Work email..."
-              onSubmit={onSendEmail}
-            />
-          )}
-        </>
-      )}
     </Line>
   )
 }
