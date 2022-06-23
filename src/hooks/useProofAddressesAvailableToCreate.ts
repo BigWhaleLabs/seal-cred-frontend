8696715f96641299fa0f9d4c719841baf8f22adf
Michel Erokhov <mixaileroxov@gmail.com>
2022-06-23 22:57:58 +0300

    update useProofAddressesAvailableToCreate

--- src/hooks/useProofAddressesAvailableToCreate.ts
+++ src/hooks/useProofAddressesAvailableToCreate.ts
@@ -4,21 +4,22 @@ import ProofStore from 'stores/ProofStore'
 import SealCredStore from 'stores/SealCredStore'
 
 export default function () {
-  const { proofsCompleted } = useSnapshot(ProofStore)
+  const { nftsProofsCompleted } = useSnapshot(ProofStore)
   const { contractsOwned } = useSnapshot(ContractsStore)
   const { erc721DerivativeContracts } = useSnapshot(SealCredStore)
-  const completedProofAddressesMap = [...proofsCompleted].reduce(
+  const completedNftsProofAddressesMap = [...nftsProofsCompleted].reduce(
     (result, proof) => ({
       ...result,
       [proof.contract]: true,
     }),
     {} as { [address: string]: boolean }
   )
+  // TODO: add workProofsCompleted
   return (
     contractsOwned.filter(
       (address) =>
         !erc721DerivativeContracts.includes(address) &&
-        !completedProofAddressesMap[address]
+        !completedNftsProofAddressesMap[address]
     ) || []
   )
 }
