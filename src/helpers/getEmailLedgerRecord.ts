951ef214f5b14bf2fe708047feb9096d0194e9d9
Michel Erokhov <mixaileroxov@gmail.com>
2022-06-27 11:38:42 +0300

    update ERC721Ledger and EmailLedger

--- src/helpers/getEmailLedgerRecord.ts
+++ src/helpers/getEmailLedgerRecord.ts
@@ -1,12 +1,6 @@
-import { SCERC721Derivative__factory } from '@big-whale-labs/seal-cred-ledger-contract'
-import defaultProvider from 'helpers/defaultProvider'
-
 export default function (derivativeContract: string, domain: string) {
   return {
     domain,
-    derivativeContract: SCERC721Derivative__factory.connect(
-      derivativeContract,
-      defaultProvider
-    ),
+    derivativeContract,
   }
 }
--- src/models/ERC721Ledger.ts
+++ src/models/ERC721Ledger.ts
@@ -1,11 +1,6 @@
-import {
-  ERC721,
-  SCERC721Derivative,
-} from '@big-whale-labs/seal-cred-ledger-contract'
-
 export default interface ERC721Ledger {
   [originalContract: string]: {
-    originalContract: ERC721
-    derivativeContract: SCERC721Derivative
+    originalContract: string
+    derivativeContract: string
   }
 }
