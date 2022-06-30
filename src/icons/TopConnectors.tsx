ebe8034b8b2552902f4394adcae2ff3addf41dbb
T_DaMER <dpancov64@gmail.com>
2022-06-30 16:57:35 +0300

    Fix lines on Safari + orb position accross the lines

--- src/icons/TopConnectors.tsx
+++ src/icons/TopConnectors.tsx
@@ -2,10 +2,11 @@ import { stroke } from 'classnames/tailwind'
 
 const strokeTertiary = stroke('stroke-tertiary')
 const strokeSecondary = stroke('stroke-secondary')
+const strokeAccent = stroke('stroke-accent')
 
 export default function () {
   return (
-    <div style={{ width: '12.625rem', height: '26.25rem' }}>
+    <div style={{ width: '12.625rem' }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -15,7 +16,7 @@ export default function () {
           x1="10"
           y1="100"
           x2="10"
-          y2="400"
+          y2="225"
           fill="none"
           className={strokeTertiary}
           stroke-linecap="round"
@@ -24,18 +25,18 @@ export default function () {
           x1="192"
           y1="100"
           x2="192"
-          y2="400"
+          y2="235"
           fill="none"
           className={strokeSecondary}
           stroke-linecap="round"
         />
         <line
-          x1="101.5"
-          x2="101.5"
+          x1="102"
+          x2="102"
           y1="0"
-          y2="300"
+          y2="190"
           fill="none"
-          stroke="url(#c)"
+          className={strokeAccent}
         />
       </svg>
     </div>
