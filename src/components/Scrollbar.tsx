6fce420d1f52ab085a433c88446b5619d671abdc
T_DaMER <dpancov64@gmail.com>
2022-07-05 18:13:54 +0300

    make it like it was

--- src/components/Scrollbar.tsx
+++ src/components/Scrollbar.tsx
@@ -28,7 +28,6 @@ export default function ({
       className={wrapperStyles}
       style={{
         marginRight: isOverflow && hasWebKit ? '-0.7rem' : '0rem',
-        marginTop: isOverflow ? '-0.1rem' : '0rem',
       }}
     >
       {(fade === 'both' || fade === 'top') && <Fade />}
