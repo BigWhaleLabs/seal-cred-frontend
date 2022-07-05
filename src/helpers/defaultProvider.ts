340978b1d7187f34daf0c71edb080975f426983f
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-04 17:55:07 -0700

    switch to alchemy provider

--- src/helpers/defaultProvider.ts
+++ src/helpers/defaultProvider.ts
@@ -1,7 +1,9 @@
 import { providers } from 'ethers'
 import env from 'helpers/env'
 
-export default new providers.JsonRpcProvider(
-  env.VITE_ETH_RPC,
-  env.VITE_ETH_NETWORK
-)
+export default env.VITE_ETH_RPC.includes('alchemy')
+  ? new providers.AlchemyProvider(
+      env.VITE_ETH_NETWORK,
+      env.VITE_ETH_RPC.replace('https://eth-goerli.g.alchemy.com/v2/', '')
+    )
+  : new providers.JsonRpcProvider(env.VITE_ETH_RPC, env.VITE_ETH_NETWORK)
