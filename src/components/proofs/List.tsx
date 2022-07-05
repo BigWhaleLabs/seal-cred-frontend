a1702fdd9da3286612177644cd195d578ad46225
T_DaMER <dpancov64@gmail.com>
2022-07-05 17:35:02 +0300

    less flex and grow

--- src/components/proofs/List.tsx
+++ src/components/proofs/List.tsx
@@ -1,25 +1,13 @@
 import { AccentText } from 'components/Text'
 import { Suspense } from 'preact/compat'
+import { space } from 'classnames/tailwind'
 import { useSnapshot } from 'valtio'
 import ERC721ProofsList from 'components/proofs/ERC721ProofsList'
 import EmailProofList from 'components/proofs/EmailProofList'
 import ListTitle from 'components/proofs/ListTitle'
 import LoadingCard from 'components/proofs/LoadingCard'
 import Scrollbar from 'components/Scrollbar'
-import classnames, {
-  display,
-  flexDirection,
-  flexGrow,
-  space,
-} from 'classnames/tailwind'
 import proofStore from 'stores/ProofStore'
-
-const proofContentBlock = classnames(
-  display('flex'),
-  flexDirection('flex-col'),
-  flexGrow('grow'),
-  space('space-y-2')
-)
 
 export default function () {
   const { proofsCompleted } = useSnapshot(proofStore)
@@ -28,7 +16,7 @@ export default function () {
     <Suspense fallback={<LoadingCard />}>
       <ListTitle />
       <Scrollbar>
-        <div className={proofContentBlock}>
+        <div className={space('space-y-2')}>
           <ERC721ProofsList />
           <EmailProofList />
         </div>
