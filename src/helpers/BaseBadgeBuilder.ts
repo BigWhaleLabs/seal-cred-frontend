e7be465eeb43a1103306076d13339f9ee90fec4d
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-08 16:25:27 -0700

    fix all ts errors

--- src/helpers/BaseBadgeBuilder.ts
+++ src/helpers/BaseBadgeBuilder.ts
@@ -1,18 +1,11 @@
-import { BigNumber, BigNumberish, ContractTransaction, Overrides } from 'ethers'
+import { BigNumber, ContractTransaction, Overrides } from 'ethers'
 import { PromiseOrValue } from '@big-whale-labs/seal-cred-ledger-contract/dist/typechain/common'
 import { Web3Provider } from '@ethersproject/providers'
 import ProofResult from 'models/ProofResult'
 
 interface SimpleLedger {
   mint(
-    originalContract: PromiseOrValue<string>,
-    a: [PromiseOrValue<BigNumberish>, PromiseOrValue<BigNumberish>],
-    b: [
-      [PromiseOrValue<BigNumberish>, PromiseOrValue<BigNumberish>],
-      [PromiseOrValue<BigNumberish>, PromiseOrValue<BigNumberish>]
-    ],
-    c: [PromiseOrValue<BigNumberish>, PromiseOrValue<BigNumberish>],
-    input: PromiseOrValue<BigNumberish>[],
+    proof: unknown,
     overrides?: Overrides & {
       from?: PromiseOrValue<string>
     }
@@ -33,13 +26,12 @@ export default abstract class BaseBadgeBuilder {
 
     // This is a hacky way to get rid of the third arguments that are unnecessary and convert to BigNumber
     // Also pay attention to array indexes
-    const tx = await this.ledger.mint(
-      message,
-      [
+    const tx = await this.ledger.mint({
+      a: [
         BigNumber.from(proofResult.proof.pi_a[0]),
         BigNumber.from(proofResult.proof.pi_a[1]),
       ],
-      [
+      b: [
         [
           BigNumber.from(proofResult.proof.pi_b[0][1]),
           BigNumber.from(proofResult.proof.pi_b[0][0]),
@@ -49,12 +41,12 @@ export default abstract class BaseBadgeBuilder {
           BigNumber.from(proofResult.proof.pi_b[1][0]),
         ],
       ],
-      [
+      c: [
         BigNumber.from(proofResult.proof.pi_c[0]),
         BigNumber.from(proofResult.proof.pi_c[1]),
       ],
-      proofResult.publicSignals.map(BigNumber.from)
-    )
+      input: proofResult.publicSignals.map(BigNumber.from),
+    })
     return tx.wait()
   }
 }
