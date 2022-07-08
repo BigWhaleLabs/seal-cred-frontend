65aef4e3f079776a8fce7f33c196095902b894bf
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-08 16:54:58 -0700

    remove mainnet ledger

--- src/helpers/sealCred.ts
+++ src/helpers/sealCred.ts
@@ -1,12 +1,6 @@
-import {
-  goerliDefaultProvider,
-  mainnetDefaultProvider,
-} from 'helpers/defaultProvider'
+import { goerliDefaultProvider } from 'helpers/defaultProvider'
 import getSealCredERC721 from 'helpers/getSealCredERC721'
 import getSealCredEmail from 'helpers/getSealCredEmail'
 
-export const MainnetERC721LedgerContract = getSealCredERC721(
-  mainnetDefaultProvider
-)
 export const ERC721LedgerContract = getSealCredERC721(goerliDefaultProvider)
 export const SCEmailLedgerContract = getSealCredEmail(goerliDefaultProvider)
