57915fa3f836bdad8ae55847d80dc89810f38367
T_DaMER <dpancov64@gmail.com>
2022-07-08 19:11:20 +0300

    fixed for cta

--- src/components/email-flow/SealCredCTM.tsx
+++ src/components/email-flow/SealCredCTM.tsx
@@ -1,4 +1,5 @@
 import { AccentText } from 'components/Text'
+import { margin } from 'classnames/tailwind'
 import { useNavigate } from 'react-router-dom'
 import Button from 'components/Button'
 import Card from 'components/Card'
@@ -7,13 +8,15 @@ export default function () {
   const navigate = useNavigate()
 
   return (
-    <Card color="tertiary" shadow onlyWrap>
-      <AccentText color="text-tertiary">
-        Create more zkBadges with SealCred.
-      </AccentText>
-      <Button type="primary" small onClick={() => navigate('/app')}>
-        Go to SealCred
-      </Button>
-    </Card>
+    <div className={margin('md:!mb-10')}>
+      <Card color="tertiary" shadow onlyWrap>
+        <AccentText color="text-tertiary">
+          Create more zkBadges with SealCred.
+        </AccentText>
+        <Button type="primary" small onClick={() => navigate('/app')}>
+          Go to SealCred
+        </Button>
+      </Card>
+    </div>
   )
 }
