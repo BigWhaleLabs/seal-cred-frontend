caf0570e8a95f4afc06c66207bd01ff3cec02600
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-09 13:07:47 -0700

    actualize requests

--- src/helpers/attestor.ts
+++ src/helpers/attestor.ts
@@ -1,66 +1,62 @@
+import Network from 'models/Network'
 import axios from 'axios'
 import env from 'helpers/env'
 
 const baseURL = `${env.VITE_VERIFY_URL}/v0.2/verify`
 
-interface SendVerifyResponse {
+interface SignatureResponse {
   signature: string
   message: string
 }
-export async function requestERC721Attestation(
+
+export async function requestAddressOwnershipAttestation(
   signature: string,
-  tokenAddress: string,
   message: string
 ) {
-  const { data } = await axios.post<SendVerifyResponse>(
-    `${baseURL}/erc721`,
+  const { data } = await axios.post<SignatureResponse>(
+    `${baseURL}/ethereum-address`,
     {
       signature,
-      tokenAddress,
       message,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
     }
   )
-
   return data
 }
 
-interface GetPublicKeyResponse {
-  x: string
-  y: string
+export async function requestBalanceAttestation(
+  tokenAddress: string,
+  network: Network,
+  ownerAddress: string
+) {
+  const { data } = await axios.post<SignatureResponse>(`${baseURL}/balance`, {
+    tokenAddress,
+    network,
+    ownerAddress,
+  })
+  return data
 }
+
+export async function requestContractMetadata(
+  network: Network,
+  tokenAddress: string
+) {
+  const { data } = await axios.post<SignatureResponse>(`${baseURL}/balance`, {
+    tokenAddress,
+    network,
+  })
+  return data
+}
+
 export async function getPublicKey() {
-  const { data } = await axios.get<GetPublicKeyResponse>(
-    `${baseURL}/eddsa-public-key`,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    }
-  )
-
+  const { data } = await axios.get<{
+    x: string
+    y: string
+  }>(`${baseURL}/eddsa-public-key`)
   return data
 }
 
-export async function sendEmail(email: string) {
-  const data = await axios.post<SendVerifyResponse>(
-    `${baseURL}/email`,
-    {
-      email,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    }
-  )
-
-  return data
+export function sendEmail(email: string) {
+  return axios.post(`${baseURL}/email`, {
+    email,
+  })
 }
