468aacc3a4f73efd9dccdb379dfbd925d30b91dd
T_DaMER <dpancov64@gmail.com>
2022-07-11 10:24:01 +0300

    fixed bottom fade thin line between content

--- src/components/Fade.tsx
+++ src/components/Fade.tsx
@@ -16,7 +16,7 @@ const fade = (bottom = true) =>
       bottom ? 'to-primary-dark' : 'to-transparent'
     ),
     position('sticky'),
-    inset(bottom ? '-bottom-1' : '-top-1', 'left-0', 'right-0'),
+    inset(bottom ? '-bottom-1.5' : '-top-1', 'left-0', 'right-0'),
     height('h-4'),
     zIndex('z-10')
   )
