bcf8d19d01517161183cd1c1b8a10932305e64eb
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-07-11 06:32:38 -0700

    fix the default provider

--- src/helpers/providers/defaultProvider.ts
+++ src/helpers/providers/defaultProvider.ts
@@ -1,11 +1,12 @@
+import { ETH_RPC, ETH_RPC_MAINNET } from '@big-whale-labs/constants'
 import { providers } from 'ethers'
 import env from 'helpers/env'
 
 export const goerliDefaultProvider = new providers.JsonRpcProvider(
-  env.VITE_ETH_RPC,
+  ETH_RPC,
   env.VITE_ETH_NETWORK
 )
 export const mainnetDefaultProvider = new providers.JsonRpcProvider(
-  env.VITE_ETH_RPC_MAINNET,
+  ETH_RPC_MAINNET,
   'mainnet'
 )
