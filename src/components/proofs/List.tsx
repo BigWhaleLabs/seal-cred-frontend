601a6248ec9fd7f966cc132b305744b5fa1ee7e3
smyzin <smyzin.work@gmail.com>
2022-07-11 20:53:50 +0300

    Loading state

--- src/components/proofs/List.tsx
+++ src/components/proofs/List.tsx
@@ -1,11 +1,24 @@
+import { Suspense } from 'preact/compat'
 import ListTitle from 'components/proofs/ListTitle'
+import LoadingCard from 'components/proofs/LoadingCard'
 import ProofList from 'components/proofs/ProofList'
+import useProofAddressesAvailableToCreate from 'hooks/useProofAddressesAvailableToCreate'
+
+function ListSuspended() {
+  const avaliableToProof = useProofAddressesAvailableToCreate()
+
+  return (
+    <>
+      <ListTitle avaliableToProof={avaliableToProof} />
+      <ProofList />
+    </>
+  )
+}
 
 export default function () {
   return (
-    <>
-      <ListTitle />
-      <ProofList />
-    </>
+    <Suspense fallback={<LoadingCard />}>
+      <ListSuspended />
+    </Suspense>
   )
 }
--- src/components/proofs/ListTitle.tsx
+++ src/components/proofs/ListTitle.tsx
@@ -1,12 +1,9 @@
-import { Suspense } from 'preact/compat'
 import { useSnapshot } from 'valtio'
 import CardTitle from 'components/CardTitle'
 import ProofStore from 'stores/ProofStore'
-import useProofAddressesAvailableToCreate from 'hooks/useProofAddressesAvailableToCreate'
 
-export function ListTitleSuspended() {
+export default function ({ avaliableToProof }: { avaliableToProof: string[] }) {
   const { proofsCompleted } = useSnapshot(ProofStore)
-  const avaliableToProof = useProofAddressesAvailableToCreate()
 
   const allGenerated =
     proofsCompleted.length > 0 && avaliableToProof.length === 0
@@ -20,20 +17,5 @@ export function ListTitleSuspended() {
           : 'Generate ZK proofs'
       }
     />
-  )
-}
-
-export default function () {
-  return (
-    <Suspense
-      fallback={
-        <CardTitle
-          title="Loading..."
-          subtitle="Please, wait until I load supported NFTs, it takes time"
-        />
-      }
-    >
-      <ListTitleSuspended />
-    </Suspense>
   )
 }
