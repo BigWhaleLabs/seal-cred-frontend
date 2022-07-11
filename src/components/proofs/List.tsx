6f1e33d3d3587ef71d45bc82add7f74a49470936
smyzin <smyzin.work@gmail.com>
2022-07-11 21:27:38 +0300

    Synchronize fix

--- src/components/proofs/List.tsx
+++ src/components/proofs/List.tsx
@@ -3,9 +3,11 @@ import ListTitle from 'components/proofs/ListTitle'
 import LoadingCard from 'components/proofs/LoadingCard'
 import ProofList from 'components/proofs/ProofList'
 import useProofAddressesAvailableToCreate from 'hooks/useProofAddressesAvailableToCreate'
+import useProofsAvailableToMint from 'hooks/useProofsAvailableToMint'
 
 function ListSuspended() {
   const avaliableToProof = useProofAddressesAvailableToCreate()
+  useProofsAvailableToMint() // used to synchronize loading with badge card
 
   return (
     <>
