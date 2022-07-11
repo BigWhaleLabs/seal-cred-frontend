5567fce06208da84b512634cc0baa910b1329d92
T_DaMER <dpancov64@gmail.com>
2022-07-11 13:36:29 +0300

    env exports constants by default, we should use them

--- src/helpers/providers/defaultProvider.ts
+++ src/helpers/providers/defaultProvider.ts
@@ -1,12 +1,11 @@
-import { ETH_RPC, ETH_RPC_MAINNET } from '@big-whale-labs/constants'
 import { providers } from 'ethers'
 import env from 'helpers/env'
 
 export const goerliDefaultProvider = new providers.JsonRpcProvider(
-  ETH_RPC,
+  env.VITE_ETH_RPC,
   env.VITE_ETH_NETWORK
 )
 export const mainnetDefaultProvider = new providers.JsonRpcProvider(
-  ETH_RPC_MAINNET,
+  env.VITE_ETH_RPC_MAINNET,
   'mainnet'
 )
