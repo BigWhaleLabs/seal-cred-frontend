db2978a2823bdb903e23abab4d717562613234c0
Michel Erokhov <mixaileroxov@gmail.com>
2022-07-26 23:35:58 +0300

    fix borderColor semi-dimmed

--- src/helpers/colors/colorToBorderColor.ts
+++ src/helpers/colors/colorToBorderColor.ts
@@ -3,8 +3,8 @@ import Color from 'models/Color'
 
 export default function (color: Color) {
   switch (color) {
-    case 'formal-accent':
-      return borderColor('border-formal-accent')
+    case 'primary-semi-dimmed':
+      return borderColor('border-primary-semi-dimmed')
     case 'formal-accent-semi-transparent':
       return borderColor('border-formal-accent-semi-transparent')
   }
