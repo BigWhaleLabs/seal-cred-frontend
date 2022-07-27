66d37910d7f7829111f4c6db7d8d46caa5777ff7
Michel Erokhov <mixaileroxov@gmail.com>
2022-07-27 13:04:51 +0300

    add wrapper around spinner

--- src/components/LazyComponent.tsx
+++ src/components/LazyComponent.tsx
@@ -1,7 +1,18 @@
 import { JSX } from 'preact'
 import { Suspense } from 'react'
+import { minHeight } from 'classnames/tailwind'
 import Loading from 'icons/Loading'
 
 export default function ({ lazyImported }: { lazyImported: JSX.Element }) {
-  return <Suspense fallback={<Loading screenCentre />}>{lazyImported}</Suspense>
+  return (
+    <Suspense
+      fallback={
+        <div className={minHeight('min-h-screen')}>
+          <Loading screenCentre />
+        </div>
+      }
+    >
+      {lazyImported}
+    </Suspense>
+  )
 }
