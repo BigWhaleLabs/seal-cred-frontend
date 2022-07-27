7aa4953440feebe8c62019caf3fc39bce4cbe385
T_DaMER <dpancov64@gmail.com>
2022-07-27 15:53:52 +0300

    Remove NULL=\u0000 from contract names

--- src/components/ContractName.tsx
+++ src/components/ContractName.tsx
@@ -60,6 +60,8 @@ function ContractNameSuspended({
   if (truncate) content = truncateMiddleIfNeeded(content, 17)
   if (utils.isAddress(content)) content = truncateMiddleIfNeeded(content, 17)
 
+  content = content.replaceAll('\u0000', '')
+
   return (
     <span
       className={classNamesToString(
