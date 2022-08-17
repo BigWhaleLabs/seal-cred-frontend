5896cae510d3a7756fc2b01a2abd1b72726cdf83
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-08-17 18:33:46 +0400

    fix type

--- src/helpers/parseGSNError.ts
+++ src/helpers/parseGSNError.ts
@@ -6,7 +6,7 @@ function transformRelayErrorMessage(message: string) {
     ?.join('\n')
 }
 
-export default function (gSNError: string): string | undefined {
+export default function (gSNError: string) {
   const metaMaskMessage = 'MetaMask Message Signature:'
   if (gSNError.includes(metaMaskMessage)) {
     return gSNError.split(metaMaskMessage)[1]?.split(' stack:')[0]
