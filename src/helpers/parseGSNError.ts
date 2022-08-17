1e2a2fed9af1b4fd94ed470267737435522cbf0a
ruslan zapevalov <ruslan@bigwhalelabs.com>
2022-08-17 18:32:01 +0400

    fix generic gsn error

---
+++ src/helpers/parseGSNError.ts
@@ -0,0 +1,15 @@
+function transformRelayErrorMessage(message: string) {
+  // Removes stack trace information
+  return message
+    .split('stack')
+    ?.filter((_, i) => i % 2 === 0)
+    ?.join('\n')
+}
+
+export default function (gSNError: string): string | undefined {
+  const metaMaskMessage = 'MetaMask Message Signature:'
+  if (gSNError.includes(metaMaskMessage)) {
+    return gSNError.split(metaMaskMessage)[1]?.split(' stack:')[0]
+  }
+  return transformRelayErrorMessage(gSNError)
+}
