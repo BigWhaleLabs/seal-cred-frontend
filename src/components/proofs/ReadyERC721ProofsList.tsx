13bd7cebf20a7c06c64272a265a45e6b999fc321
Michel Erokhov <mixaileroxov@gmail.com>
2022-08-23 09:51:55 +0300

    rename const inside ReadyERC721ProofsList

--- src/components/proofs/ReadyERC721ProofsList.tsx
+++ src/components/proofs/ReadyERC721ProofsList.tsx
@@ -8,15 +8,15 @@ import ProofStore from 'stores/ProofStore'
 export default function ({ network }: { network: Network }) {
   const { proofsCompleted } = useSnapshot(ProofStore)
 
-  const filtered = proofsCompleted.filter(
+  const allERC721ProofsByNetwork = proofsCompleted.filter(
     (proof) => proof instanceof ERC721Proof && proof.network === network
   ) as ERC721Proof[]
 
   return (
     <>
-      {!!filtered?.length && (
+      {!!allERC721ProofsByNetwork?.length && (
         <ContractListContainer>
-          {Array.from(filtered)
+          {Array.from(allERC721ProofsByNetwork)
             .sort((a, b) => (a.account === b.account ? 0 : -1))
             .map((proof) => (
               <Proof
