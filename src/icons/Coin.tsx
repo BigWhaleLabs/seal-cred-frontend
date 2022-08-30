cd393e061cb95ce5ed49a574e00441f480f5ad7c
T_DaMER <dpancov64@gmail.com>
2022-08-30 17:52:57 +0300

    moved function outside of the component

--- src/icons/Coin.tsx
+++ src/icons/Coin.tsx
@@ -1,12 +1,13 @@
 import CommonIconsProps from 'icons/CommonIconsProps'
 import classnames, { stroke, strokeWidth } from 'classnames/tailwind'
 
-export default function ({ inheritStrokeColor }: CommonIconsProps) {
-  const commonPathStyles = classnames(
+const commonPathStyles = (inheritStrokeColor?: boolean) =>
+  classnames(
     stroke(inheritStrokeColor ? 'stroke-inherit' : 'stroke-primary-background'),
     strokeWidth('stroke-2')
   )
 
+export default function ({ inheritStrokeColor }: CommonIconsProps) {
   return (
     <svg
       width="24"
@@ -16,14 +17,14 @@ export default function ({ inheritStrokeColor }: CommonIconsProps) {
     >
       <path
         d="M11 5L14 5C17.866 5 21 8.13401 21 12C21 15.866 17.866 19 14 19L11 19"
-        className={commonPathStyles}
+        className={commonPathStyles(inheritStrokeColor)}
       />
       <circle
         cx="11"
         cy="12"
         r="7"
         transform="rotate(-90 11 12)"
-        className={commonPathStyles}
+        className={commonPathStyles(inheritStrokeColor)}
       />
     </svg>
   )
