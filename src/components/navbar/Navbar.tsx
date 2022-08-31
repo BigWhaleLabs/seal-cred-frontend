155813fd3ba3eb9f0b3d486b00ea9abaf329d347
T_DaMER <dpancov64@gmail.com>
2022-08-31 13:00:50 +0300

    better navbar movement when resizing

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -27,7 +27,7 @@ const navbar = (visible?: boolean, withoutWallet?: boolean) =>
     display('flex'),
     alignItems('items-center'),
     justifyContent(withoutWallet ? 'sm:justify-center' : 'justify-between'),
-    padding('py-4', 'px-4', 'lg:px-25'),
+    padding('py-4', 'px-4', 'sm:px-8', '2xl:!px-25'),
     space('xs:space-x-4', 'sm:space-x-9', 'lg:space-x-0'),
     zIndex('z-50'),
     backgroundColor(visible ? 'bg-primary-dark' : 'bg-transparent'),
