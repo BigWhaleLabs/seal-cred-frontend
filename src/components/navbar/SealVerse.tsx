683f0c37f5cf9dc921ec67090944904dbeba59d1
T_DaMER <dpancov64@gmail.com>
2022-09-07 15:49:34 +0300

    proper import

--- src/components/navbar/SealVerse.tsx
+++ src/components/navbar/SealVerse.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from 'react-router-dom'
 import Dropdown from 'components/Dropdown'
-import sealVerseData from 'sealverseData'
+import sealVerseData from 'sealVerseData'
 
 export default function () {
   const location = useLocation()
