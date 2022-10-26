6d867f8c59757985026282f925f9cfc08a0705bb
T_DaMER <dpancov64@gmail.com>
2022-10-26 21:57:04 +0300

    extracted into const

--- src/icons/Cross.tsx
+++ src/icons/Cross.tsx
@@ -8,6 +8,13 @@ import {
   transitionProperty,
 } from 'classnames/tailwind'
 
+const animationWrapper = classnames(
+  borderRadius('rounded-full'),
+  backgroundColor('hover:bg-primary-semi-dimmed'),
+  transitionDuration('duration-200'),
+  transitionProperty('transition-colors')
+)
+
 export default function ({ inheritColor }: { inheritColor?: boolean }) {
   const strokeStyle = classnames(
     strokeWidth('stroke-2'),
@@ -15,14 +22,7 @@ export default function ({ inheritColor }: { inheritColor?: boolean }) {
   )
 
   return (
-    <div
-      className={classnames(
-        borderRadius('rounded-full'),
-        backgroundColor('hover:bg-primary-semi-dimmed'),
-        transitionDuration('duration-200'),
-        transitionProperty('transition-colors')
-      )}
-    >
+    <div className={animationWrapper}>
       <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <line className={strokeStyle} x1="5" y1="5" x2="19.1" y2="18.6" />
         <line className={strokeStyle} x1="5" y1="18.6" x2="18.6" y2="5" />
