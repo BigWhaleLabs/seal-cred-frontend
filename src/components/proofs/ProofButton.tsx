230a5eba78af82939342f408a3d4383619ba279a
T_DaMER <dpancov64@gmail.com>
2022-10-27 12:08:08 +0300

    remake proof button

--- src/components/proofs/ProofButton.tsx
+++ src/components/proofs/ProofButton.tsx
@@ -14,20 +14,19 @@ import {
   transitionProperty,
 } from 'classnames/tailwind'
 
-const button = (disabled?: boolean) =>
-  classnames(
-    display('flex'),
-    flexDirection('flex-row'),
-    space('space-x-2'),
-    alignItems('items-center'),
-    fontWeight('font-bold'),
-    transitionProperty('transition-colors'),
-    fontFamily('font-primary'),
-    outlineStyle('focus:outline-none'),
-    cursor(disabled ? 'cursor-not-allowed' : undefined),
-    opacity(disabled ? 'opacity-95' : undefined),
-    lineHeight('leading-5')
-  )
+const button = classnames(
+  display('flex'),
+  flexDirection('flex-row'),
+  space('space-x-2'),
+  alignItems('items-center'),
+  fontWeight('font-bold'),
+  transitionProperty('transition-colors'),
+  fontFamily('font-primary'),
+  outlineStyle('focus:outline-none'),
+  cursor('disabled:cursor-not-allowed'),
+  opacity('disabled:opacity-95'),
+  lineHeight('leading-5')
+)
 
 export default function ({
   children,
@@ -35,7 +34,7 @@ export default function ({
   ...rest
 }: HTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className={button(!!disabled)} disabled={disabled} {...rest}>
+    <button className={button} disabled={disabled} {...rest}>
       {typeof children === 'string' ? <>{children}</> : children}
     </button>
   )
